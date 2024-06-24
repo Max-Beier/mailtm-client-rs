5b584afdd03b86c08a1734e6b01a166f171c5b72
Max-Beier <max@maxbeier.dev>
2024-06-24 23:25:39 +0200

    feat(credentials) added constructor

--- src/account_credentials.rs
+++ src/account_credentials.rs
@@ -2,3 +2,12 @@ pub struct AccountCredentials {
     pub username: String,
     pub password: String,
 }
+
+impl AccountCredentials {
+    pub fn new(username: &str, password: &str) -> Self {
+        Self {
+            username: username.to_string(),
+            password: password.to_string(),
+        }
+    }
+}
